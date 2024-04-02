c079c0da98b33f961a20fae44f211ae19f602d54
Jerry-XuX <49436129+Jerry-XuX@users.noreply.github.com>
2024-04-02 15:28:28 +0800

    LeetCode:Merge Two Sorted Lists

--- main.cpp
+++ main.cpp
@@ -1,101 +1,114 @@
-#include <string>
-#include <iostream>
-#include <map>
-#include <vector>
-#include <stack>
-// (),[],{}
-class Solution
-{
-public:
-	Solution() {
-		map['('] = -1;
-		map[')'] = 1;
-		map['['] = -2;
-		map[']'] = 2;
-		map['{'] = -3;
-		map['}'] = 3;
-	}
-public:
-	std::map<char, int> map;
-public:
-	
-	bool isValid(std::string s) {
-		std::stack<int> st;
-		for (int i = 0; i < s.size(); i++)
-		{
-			std::cout << s[i] << std::endl;
-			if (s[i] == '(' || s[i] == '[' || s[i] == '{')
-			{
-				st.push(i);
-			}
-			else
-			{
-				if (st.empty())
-				{
-					return false;
-				}
-				if (s[i] == ')'&&s[st.top()] != '(')
-				{
-					return false;
-				}
-				if (s[i] == '}'&&s[st.top()] != '{')
-				{
-					return false;
-				}
-				if (s[i] == ']'&&s[st.top()] != '[')
-				{
-					return false;
-				}
-				st.pop();
-			}
-		}
-		return st.empty();
-	}
+#include<vector>
+#include<map>
+#include<iostream>
 
-	bool isValidMyAnswer(std::string s)
-	{
-		std::stack<char> stk;
-		for (int i = 0; i < s.size(); i++)
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() :val(0), next(nullptr) {}
+	ListNode(int x) :val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) :val(x), next(next) {}
+};
+
+class Solution {
+public:
+
+public:
+	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+		std::vector<int> vList1;
+		std::vector<int> vList2;
+		std::vector<int> vBool;// List1->0, List2->1
+
+		ListNode* listTmp1 = list1;
+		while (listTmp1)
 		{
-			auto auto1 = map.find(s[i]);
-			if (auto1->second < 0)
+			vList1.push_back(listTmp1->val);
+			listTmp1 = listTmp1->next;
+		}
+
+		ListNode* listTmp2 = list2;
+		while (listTmp2)
+		{
+			vList2.push_back(listTmp2->val);
+			listTmp2 = listTmp2->next;
+		}
+
+		while (!vList1.empty() || !vList2.empty())
+		{
+			if (!vList1.empty() && !vList2.empty())
 			{
-				stk.push(s[i]);
+				if (vList1[0] > vList2[0])
+				{
+					vList2.erase(vList2.begin());
+					vBool.push_back(0);
+				}
+				else if (vList1[0] == vList2[0])
+				{
+					vList1.erase(vList1.begin());
+					vList2.erase(vList2.begin());
+					vBool.push_back(0);
+					vBool.push_back(1);
+				}
+				else
+				{
+					vList1.erase(vList1.begin());
+					vBool.push_back(1);
+				}
+			}
+			else if (vList1.empty())
+			{
+				vList2.erase(vList2.begin());
+				vBool.push_back(0);
 			}
 			else
 			{
-				if (stk.empty()) return false;
-				auto auto2 = map.find(s[i]);
-				if (auto1->second + auto2->second != 0)
-				{
-					return false;
-				}
-				stk.pop();
+				vList1.erase(vList1.begin());
+				vBool.push_back(1);
 			}
 		}
-		return stk.empty();
+
+		ListNode* ListDummy = new ListNode(0, nullptr);
+		ListNode* ListRes = ListDummy;
+		for (int n = 0; n < vBool.size(); n++)
+		{
+			if (vBool[n])
+			{
+				ListDummy->next = list1;
+				ListDummy = ListDummy->next;
+				list1 = list1->next;
+			}
+			else
+			{
+				ListDummy->next = list2;
+				ListDummy = ListDummy->next;
+				list2 = list2->next;
+			}
+		}
+		//ListNode* listRes = new ListNode(0, ListDummy);
+		return ListRes->next;
 	}
 };
 
 int main()
 {
-	//std::string s = "([)]";
-	//std::string s = "()";
-	//std::string s = "(([]){})";
-	//std::string s = "[({(())}[()])]";
-	//std::string s = "(]";
-	//std::string s = "(){}}{";
-	std::string s = "{}[{}]((){})(){}";
+	//ListNode* list1 = new ListNode(1, nullptr);
+	//list1->next = new ListNode(2);
+	//list1->next->next = new ListNode(4);
+
+	//ListNode* list2 = new ListNode(1);
+	//list2->next = new ListNode(3);
+	//list2->next->next = new ListNode(4);
+
+	ListNode* list1 = new ListNode();
+	list1 = nullptr;
+	ListNode* list2 = new ListNode(0, nullptr);
 	Solution sol;
-	if (sol.isValid(s))
+	ListNode* listRes =  sol.mergeTwoLists(list1, list2);
+	std::cout << "show listRes: " << std::endl;
+	while (listRes)
 	{
-		std::cout << "true" << std::endl;
+		std::cout << listRes->val << std::endl;
+		listRes = listRes->next;
 	}
-	else
-	{
-		std::cout << "false" << std::endl;
-	}
-	std::stack<char> stk;
-	stk.push('a');
 	return 0;
 }
